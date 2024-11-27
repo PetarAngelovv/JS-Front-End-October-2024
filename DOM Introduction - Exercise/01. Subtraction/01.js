ab378c0677fe52686abf1c2316df94e5be6a09ca
Petar_Angelov <101021042+PetarAngelovv@users.noreply.github.com>
2024-11-27 08:42:56 +0200

    Add files via upload

---
+++ DOM Introduction - Exercise/01. Subtraction/01.js
@@ -0,0 +1,5 @@
+function subtract() {
+  const FirstNum = Number(document.querySelector('#firstNumber').value);
+  const secondNum= Number(document.querySelector('#secondNumber').value);
+ document.querySelector('#result').textContent = (FirstNum - secondNum);
+}
---
+++ DOM Introduction - Exercise/04. Search in List/04.js
@@ -0,0 +1,19 @@
+function searchInList() {
+  const towns = document.querySelectorAll('#towns li');
+  const searchEl =  document.querySelector('#searchText').value.toLowerCase();
+  const resultEl = document.querySelector('#result');
+
+ if(searchEl == '') return;
+
+ towns.forEach(town => {
+  town.classList.remove('match');
+  town.style.fontWeight = 'normal';
+  town.style.textDecoration = 'none';
+  if(town.textContent.toLowerCase().includes(searchEl)){
+     town.classList.add('match');
+     town.style.fontWeight = 'bolder';
+     town.style.textDecoration = 'underline';
+  }
+ });
+resultEl.textContent = `${document.querySelectorAll('.match').length} matches found`;
+}  
