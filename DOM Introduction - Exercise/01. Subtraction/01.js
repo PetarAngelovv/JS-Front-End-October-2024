22b20b96d2186250f475b5acb8ed04d9948ebcab
Petar_Angelov <101021042+PetarAngelovv@users.noreply.github.com>
2024-11-28 12:43:24 +0200

    Delete DOM Introduction - Exercise directory

--- DOM Introduction - Exercise/01. Subtraction/01.js
+++
@@ -1,5 +0,0 @@
-function subtract() {
-  const FirstNum = Number(document.querySelector('#firstNumber').value);
-  const secondNum= Number(document.querySelector('#secondNumber').value);
- document.querySelector('#result').textContent = (FirstNum - secondNum);
-}
